3f37bd81c5d78b9c0fda264d025956c3533b5613
Jason Zhang <zjjc123@hotmail.com>
2022-08-15 10:05:18 -0700

    Click to Update Image

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,12 +1,32 @@
-import "../styles/video.css"
+import "../styles/video.css";
+import React, { useState, useRef } from "react";
 
 export const Video = () => {
+  const [file, setFile] = useState<string>("");
+  const fileHandler = useRef<HTMLInputElement>(null)
+
+  const openFileHandler = () => {
+    fileHandler.current?.click()
+  }
+
+  const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(file)
+    if (e.target.files) setFile(URL.createObjectURL(e.target.files[0]));
+    console.log(e.target.files)
+  };
+
   return (
     <article className="video-container">
-      <a href="#" className="thumbnail" data-duration="12:24">
+      <input className="hidden" type="file" onChange= {e => handleFile(e)} ref={fileHandler}/>
+      <a
+        href="#"
+        className="thumbnail"
+        data-duration="12:24"
+        onClick={openFileHandler}
+      >
         <img
           className="thumbnail-image"
-          src="http://unsplash.it/250/150?gravity=center"
+          src={file !== ""? file : "http://unsplash.it/250/150?gravity-center"}
         ></img>
       </a>
       <div className="video-bottom-section">
